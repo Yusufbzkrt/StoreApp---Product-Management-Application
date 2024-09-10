5b88d17008213eb8e24cd35f9e2dee79f127670f
Yusufbzkrt <yusuf.bozkurt1593@gmail.com>
2024-09-10 09:31:55 +0300

    Add project files.

---
+++ Repositories/RepositoryContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Entities.Models;
+namespace Repositories
+{
+	public class RepositoryContext : DbContext //veitabanı gibi işlem görür.
+	{
+		public DbSet<Product> Products { get; set; }
+
+		public RepositoryContext(DbContextOptions<RepositoryContext> options)
+			: base(options)
+		{
+
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<Product>()
+				.HasData(
+				new Product() { ProductId = 1, ProductName = "Bilgisayar", Price = 17_000 },
+				new Product() { ProductId = 2, ProductName = "Klavye", Price = 1_000 },
+				new Product() { ProductId = 3, ProductName = "Mouse", Price = 500 },
+				new Product() { ProductId = 4, ProductName = "Monitor", Price = 4_000 },
+				new Product() { ProductId = 5, ProductName = "Kasa", Price = 15_000 },
+				new Product() { ProductId = 6, ProductName = "SSD", Price = 2_000 }
+				);
+		}
+
+	}
+}
---
+++ StoreApp/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
+using Repositories;
+
+namespace StoreApp.Controllers
+{
+	public class ProductController : Controller
+	{
+		private readonly RepositoryContext _Context;//9-15 arası anlatılmak istenilen, repositorycontext görüldüğü anda bağlantı dizesi direk oluşturulsun ve kullanabileceğim bir context ifadesi versin.
+
+		public ProductController(RepositoryContext context)
+		{
+			_Context = context;
+		}
+
+		public IActionResult Index()
+		{
+			var model = _Context.Products.ToList();
+			return View(model);
+		}
+
+		public IActionResult Get(int id)
+		{
+			var model = _Context.Products.FirstOrDefault(p=> p.ProductId.Equals(id));
+			return View(model);
+		} 
+	}
+}
