e1f5c415bbb26e4fa3012d7dbd563eedbff0eaea
Yusufbzkrt <yusuf.bozkurt1593@gmail.com>
2024-09-11 11:12:04 +0300

    Adding components

---
+++ Services/CategoryManager.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Models;
+using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+	public class CategoryManager : ICategoryService
+	{
+		private readonly IRepositoryManager _manager;//13-18 arası veritbanına erişim
+
+		public CategoryManager(IRepositoryManager manager)
+		{
+			_manager = manager;
+		}
+
+		public IEnumerable<Category> GetAllCategories(bool trackChanges)
+		{
+			return _manager.Category.FindAll(trackChanges);
+		}
+	}
+}
---
+++ StoreApp/Components/ProductSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repositories;
+using Repositories.Contracts;
+using Services.Contracts;
+
+namespace StoreApp.Components
+{//kaç ürün varsa sayısını dönsün
+	public class ProductSummaryViewComponent : ViewComponent
+	{
+		private readonly IServiceManager _manager;
+
+		public ProductSummaryViewComponent(IServiceManager manager)
+		{
+			_manager = manager;
+		}
+
+		public string Invoke()
+		{
+			return _manager.ProductService.GetAllProducts(false).Count().ToString();
+		}
+	}
+}
