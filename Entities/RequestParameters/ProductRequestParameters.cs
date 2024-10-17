8542235b1e4a5d99f4dff53bc3ca233eff8fa8ff
Yusufbzkrt <yusuf.bozkurt1593@gmail.com>
2024-10-17 21:46:14 +0300

    Taghelpers process

--- Entities/RequestParameters/ProductRequestParameters.cs
+++ Entities/RequestParameters/ProductRequestParameters.cs
@@ -12,5 +12,18 @@ namespace Entities.RequestParameters
         public int MinPrice { get; set; } = 0;
 		public int MaxPrice { get; set; } = int.MaxValue;
         public bool IsValidPrice => MaxPrice > MinPrice;//maxprice min den büyük değilse false döner
-    }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+
+        public ProductRequestParameters() :this(1,6)
+        {
+            
+        }
+
+        public ProductRequestParameters(int pageNumber, int pageSize)
+		{
+			PageNumber = pageNumber;
+			PageSize = pageSize;
+		}
+	}
 }
--- Repositories/Extensions/ProductRepositoryExtensions.cs
+++ Repositories/Extensions/ProductRepositoryExtensions.cs
@@ -49,5 +49,14 @@ namespace Repositories.Extensions
 				return products;
             }
         }
+
+		public static IQueryable<Product> ToPaginate(this IQueryable<Product> products,
+			int pageNumber, int pageSize)
+		{
+
+			return products
+				.Skip(((pageNumber-1)*pageSize))
+				.Take(pageSize);
+		}
 	}
 }
